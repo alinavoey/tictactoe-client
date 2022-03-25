cd596a2ab109d17cc751ea0e12647daad27cf275
Alina <voey.alina@gmail.com>
2022-03-25 15:31:00 -0400

    added API configs to URLs

--- app/auth/api.js
+++ app/auth/api.js
@@ -1,9 +1,10 @@
 const store = require('../store.js')
+const config = require('../config.js')
 
 const signUp = function(data) {
     return $.ajax({
         method: 'POST',
-        url: 'https://tic-tac-toe-api-development.herokuapp.com/sign-up',
+        url: config.apiUrl + '/sign-up',
         data
     })
 }
@@ -11,7 +12,7 @@ const signUp = function(data) {
 const signIn = function(data) {
     return $.ajax({
         method: 'POST',
-        url: 'https://tic-tac-toe-api-development.herokuapp.com/sign-in',
+        url: config.apiUrl + '/sign-in',
         data
     })
 }
@@ -19,7 +20,7 @@ const signIn = function(data) {
 const changePw = function(data) {
     return $.ajax({
         method: 'PATCH',
-        url: 'https://tic-tac-toe-api-development.herokuapp.com/change-password',
+        url: config.apiUrl + '/change-password',
         headers: {
             Authorization: 'Bearer ' + store.user.token
         },
@@ -30,7 +31,7 @@ const changePw = function(data) {
 const signOut = function() {
     return $.ajax({
         method: 'DELETE',
-        url: 'https://tic-tac-toe-api-development.herokuapp.com/sign-out',
+        url: config.apiUrl + '/sign-out',
         headers: {
             Authorization: 'Bearer ' + store.user.token
         }
--- app/games/api.js
+++ app/games/api.js
@@ -1,9 +1,10 @@
 const store = require('../store.js')
+const config = require('../config.js')
 
 const gameStart = function (){
     return $.ajax({
     method: "POST",
-    url: "https://tic-tac-toe-api-development.herokuapp.com/games",
+    url: config.apiUrl + "/games",
     headers: {
         Authorization: 'Bearer ' + store.user.token
     },
